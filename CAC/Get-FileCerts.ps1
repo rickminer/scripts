04c1031d404a4c05b96072518c9c2d7f2402e43c
Rick Miner <rick.miner@noaa.gov>
2020-03-09 12:38:56 -0400

    Bug fix and testing.

--- CAC/Get-FileCerts.ps1
+++ CAC/Get-FileCerts.ps1
@@ -5,7 +5,7 @@ function Get-FileCerts {
 #    $users = Get-WmiObject -ClassName Win32_UserProfile | Where-Object { $_.LocalPath -like '*\Users*' } | select @{LABEL='Username';EXPRESSION={(Get-WmiObject Win32_UserAccount -Filter "SID = '$($_.Sid)'").Caption}},LocalPath,@{LABEL='ComputerName';EXPRESSION={$_.PSComputerName}}
 
     # Only get AD accounts
-    $users = Get-WmiObject -ClassName Win32_UserProfile | Where-Object { $_.LocalPath -like '*\Users*' } | Select-Object @{LABEL='Username';EXPRESSION={(Get-ADObject -Filter "objectSid -eq '$($_.SID)'").Name}},LocalPath,@{LABEL='ComputerName';EXPRESSION={$_.PSComputerName}},@{LABEL='DistinguisehdName';EXPRESSION={(Get-ADObject -Filter "objectSid -eq '$($_.SID)'").DistinguishedName}} | Where-Object { $_.Username }
+    $users = Get-WmiObject -ClassName Win32_UserProfile | Where-Object { $_.LocalPath -like '*\Users*' } | Select-Object @{LABEL='Username';EXPRESSION={([ADSI]"LDAP://<SID=$($_.SID)>").SamAccountName}},LocalPath,@{LABEL='ComputerName';EXPRESSION={$_.PSComputerName}},@{LABEL='DistinguishedName';EXPRESSION={([ADSI]"LDAP://<SID=$($_.SID)>").DistinguishedName}} | Where-Object { $_.Username }
     $users | ForEach-Object {
 #        echo "User: $($_)"
         $path = "$($_.LocalPath)\AppData\Roaming\Microsoft\SystemCertificates\My\Certificates"
@@ -33,11 +33,11 @@ function Get-FileCerts {
         }
 
         $CertInfo = [PSCustomObject]@{
-            UPN = $UPN
-            Email = $EMAIL
             Username = $_.Username
+            Email = $EMAIL
             DistinguishedName = $_.DistinguishedName
             Computer = $_.ComputerName
+            UPN = $UPN
             Subject = $SUBJECT
             Issuer = $ISSUER
         }
@@ -51,4 +51,4 @@ function Get-FileCerts {
 
 #$data = 
 #Get-FileCerts | Export-Csv -Append -NoTypeInformation -Path CAC.csv
-Get-FileCerts
+Get-FileCerts 
